a25d02e14798cd24ceec491eb8fcb8f206ce3f1f
sakis475 <sakis475@users.noreply.github.com>
2019-06-01 22:53:13 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,6 +1,152 @@
+import plex  
 
-import plex
+class ParseError (Exception):
+    pass
 
+class myParser:
+    def __init__(self):
+        #ta pattern pou xreiazomaste
+        string = plex.Range("azAZ")
+        token_equals = plex.Str('=')
+        token_leftParenthesis = plex.Str('(')
+        token_rightParenthesis = plex.Str(')')
+        number = plex.Range('01')
+        space = plex.Any(' \n\t')
+        token_xor = plex.Str('xor')
+        token_or = plex.Str('or')
+        token_and = plex.Str('and')
+        identifier = string + plex.Rep(string|number)
+        binary = plex.Rep1(number)
+        token_print = plex.Str('print')
+        #to scanner leksiko
+        self.lexicon = plex.Lexicon([
+            (token_xor, 'xor'),
+            (token_or, 'or'),
+            (token_and, 'and'),
+            (token_leftParenthesis, '('),
+            (token_rightParenthesis, ')'),
+            (token_equals, '='),
+            (binary, 'binary'),
+            (token_print, 'print'),
+            (identifier, 'id'),
+            (space, plex.IGNORE),
+        ])
 
-# ... συμπληρώστε τον κώδικά σας για τον συντακτικό αναλυτή - αναγνωριστή της γλώσσας ...
+    def create_scanner(self, fp):
+        """ Creates a plex scanner for a particular grammar 
+        to operate on file object fp. """
+        self.scanner = plex.Scanner(self.lexicon, fp)
+        self.la, self.text = self.next_token()
+    
+    def next_token(self):
+        """ Returns tuple (next_token,matched-text). """
+        return self.scanner.read()
 
+    def position(self):
+        """ Utility function that returns position in text in case of errors.
+        Here it simply returns the scanner position. """
+        
+        return self.scanner.position()
+
+    
+
+    def match(self, token):
+        """ Consumes (matches with current lookahead) an expected token.
+        Raises ParseError if anything else is found. Acquires new lookahead. """ 
+        print(token),
+        if self.la == token:
+            self.la, self.text = self.next_token()
+        else:
+            raise ParseError("Expected self.la == token in match()")
+
+    def parse(self, fp):
+        self.create_scanner(fp)
+        self.expression_list()
+
+    def expression_list(self):
+        if self.la == 'id' or self.la == 'print':
+            self.expression()
+            self.expression_list()
+        elif self.la == None:
+            return
+        else:
+            raise ParseError('Expected id or print in expression_list()')
+
+    def expression(self):
+        if self.la == 'print':
+            self.match('print')
+            self.definition()
+
+        elif self.la == 'id':
+              self.match('id')
+              self.match('=')
+              self.definition()
+        else:
+            raise ParseError('Expected id or print in expression()')
+
+    def definition(self):
+        if self.la == 'id' or self.la == 'binary' or self.la == '(':
+            self.atom()
+            self.atom_tail()
+        else:
+            raise ParseError('Expected id or binary or ( in definition()')
+    
+    def atom_tail(self):
+        if self.la == 'xor':
+            self.match('xor')
+            self.atom()
+            self.atom_tail()
+        elif self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+            return
+        else:
+            raise ParseError('Expected xor in atom_tail()')
+    
+    def atom(self):
+        if self.la == '(' or self.la == 'id' or self.la == 'binary':
+            self.term()
+            self.term_tail()
+        else:
+            raise ParseError('Expected ( or id or binary in term()')
+
+    def term_tail(self):
+        if self.la == 'or':
+            self.match('or')
+            self.term()
+            self.term_tail()
+        elif self.la == 'xor' or self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+            return
+        else:
+            raise ParseError('Expected or in term_tail()')
+
+    def term(self):
+        if self.la == '(' or self.la == 'id' or self.la == 'binary':
+            self.factor()
+            self.factor_tail()
+        else:
+            raise ParseError('Expected ( or id or binary in term()')
+    
+    def factor_tail(self):
+        if self.la == 'and':
+            self.match('and')
+            self.factor()
+            self.factor_tail()
+        elif self.la == 'xor' or self.la == 'or' or self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+            return
+        else:
+            raise ParseError('Expected and in factor_tail()')
+
+    def factor(self):
+        if self.la == '(':
+            self.match('(')
+            self.definition()
+            self.match(')')
+        elif self.la == 'id':
+            self.match('id')
+        elif self.la == 'binary':
+            self.match('binary')
+        else:
+            raise ParseError('Expected ( or id or binary in factor()')
+
+parser = myParser()
+with open('testing.txt', 'r') as fp:
+    parser.parse(fp)
--- runner.py
+++ runner.py
@@ -1,6 +1,141 @@
+import plex  
 
-import plex
+class ParseError (Exception):
+    pass
 
+class RunError (Exception):
+    pass
 
-# ... συμπληρώστε τον κώδικά σας για τον συντακτικό αναλυτή - διερμηνευτή της γλώσσας ...
+class myParser:
+    def __init__(self):
+        #ta pattern pou xreiazomaste
+        
+        string = plex.Range("azAZ")
+        token_equals = plex.Str('=')
+        token_leftParenthesis = plex.Str('(')
+        token_rightParenthesis = plex.Str(')')
+        number = plex.Range('01')
+        space = plex.Any(' \n\t')
+        token_xor = plex.Str('xor')
+        token_or = plex.Str('or')
+        token_and = plex.Str('and')
+        identifier = string + plex.Rep(string|number)
+        binary = plex.Rep1(number)
+        token_print = plex.Str('print')
+        #to scanner leksiko
+        self.lexicon = plex.Lexicon([
+            (token_xor, 'xor'),
+            (token_or, 'or'),
+            (token_and, 'and'),
+            (token_leftParenthesis, '('),
+            (token_rightParenthesis, ')'),
+            (token_equals, '='),
+            (binary, 'binary'),
+            (token_print, 'print'),
+            (identifier, 'id'),
+            (space, plex.IGNORE),
+        ])
+        self.st = {}
 
+    def create_scanner(self, fp):
+        """ Creates a plex scanner for a particular grammar 
+        to operate on file object fp. """
+        self.scanner = plex.Scanner(self.lexicon, fp)
+        self.la, self.text = self.next_token()
+    
+    def next_token(self):
+        """ Returns tuple (next_token,matched-text). """
+        return self.scanner.read()
+
+    def parse(self, fp):
+        self.create_scanner(fp)
+        self.expression_list()
+
+    def match(self, token):
+        """ Consumes (matches with current lookahead) an expected token.
+        Raises ParseError if anything else is found. Acquires new lookahead. """ 
+        if self.la == token:
+            self.la, self.text = self.next_token()
+        else:
+            raise ParseError("Expected self.la == token in match()")
+
+    def expression_list(self):
+        if self.la == 'id' or self.la == 'print':
+            self.expression()
+            self.expression_list()
+        elif self.la == None:
+            return
+        else:
+            raise ParseError('Expected id or print in expression_list()')
+
+    def expression(self):
+        if self.la == 'id':
+            varname = self.text
+            self.match('id')
+            self.match('=')
+            e = self.definition()
+            self.st[varname] = e
+        elif self.la == 'print':
+            self.match('print')
+            print('{:b}'.format(self.definition()))
+        else:
+            raise ParseError('Expected id or print in expression()')
+
+    def definition(self):
+        if self.la == '(' or self.la == 'id' or self.la == 'binary':
+            a = self.atom()
+            while self.la == 'xor':
+                self.match('xor')
+                a ^= self.atom()
+            if self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+                return a
+            raise ParseError('Expected xor in atom()')
+        else:
+            raise ParseError('Expected ( or id or binary in definition()')
+   
+    def atom(self):
+        if self.la == '(' or self.la == 'id' or self.la == 'binary':
+            t = self.term()
+            while self.la == 'or':
+                self.match('or')
+                t |= self.term()
+            if self.la == 'xor' or self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+                return t
+            raise ParseError('Expected or in atom()')
+        else:
+            raise ParseError('Expected ( or id or binary in atom()')
+
+    def term(self):
+        if self.la == '(' or self.la == 'id' or self.la == 'binary':
+            f = self.factor()
+            while self.la == 'and':
+                self.match('and')
+                f &= self.factor()
+            if self.la == 'xor' or self.la == 'or' or self.la == 'id' or self.la == 'print' or self.la == None or self.la == ')':
+                return f
+            raise ParseError('Expected and in term()')
+        else:
+            raise ParseError('Expected ( or id or binary in term()')
+
+    def factor(self):
+        if self.la == '(':
+            self.match('(')
+            e = self.definition()
+            self.match(')')
+            return e
+        elif self.la == 'id':
+            varname = self.text
+            self.match('id')
+            if varname in self.st:
+                return self.st[varname]
+            raise RunError("lathos sto factor")
+        elif self.la == 'binary':
+            value = int(self.text, 2)
+            self.match('binary')
+            return value
+        else:
+            raise ParseError('Expected ( or id or binary in factor()')
+
+parser = myParser()
+with open('testing.txt', 'r') as fp:
+    parser.parse(fp)
